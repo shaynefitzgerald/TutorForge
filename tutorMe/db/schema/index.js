02477e7b9fb3f7877c72d91ef6652a1c9e36169a
Shayne Fitzgerald <slfitzge@stetson.edu>
2015-10-21 20:48:12 -0400

    fixed a error in schema loading

--- tutorMe/db/schema/index.js
+++ tutorMe/db/schema/index.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 module.exports = [];
 var files = fs.readDirSync('/');
 for(var x = 0; x < files.length; x++){
-  if(files[x].substr(files[x].length-2, files[x].length) == '.js'){
+  if(files[x].substr(files[x].length-3, files[x].length) == '.js'){
     exports.push(require('./' + files[x]));
   }
 }
