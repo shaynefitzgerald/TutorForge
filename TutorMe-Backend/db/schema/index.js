ce640d065e6216c8f49a1aa374a541661afc2575
Shayne Fitzgerald <slfitzge@stetson.edu>
2015-10-27 21:38:11 -0400

    testing cas

--- TutorMe-Backend/db/schema/index.js
+++ TutorMe-Backend/db/schema/index.js
@@ -3,6 +3,6 @@ exports.dir = [];
 var files = fs.readdirSync('./db/schema/');
 for(var x = 0; x < files.length; x++){
   if(files[x].substr(files[x].length-3, files[x].length) == '.js'){
-    exports.dir = push(require('./' + files[x]));
+    exports.dir.push(require('./' + files[x]));
   }
 }
