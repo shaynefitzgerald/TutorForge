22fb5799464f47f2a21d2c21b08129432769568d
Shayne Fitzgerald <slfitzge@stetson.edu>
2015-11-01 22:26:53 -0500

    added support for serving documentation for API extensions. NOTE: REMOVE BEFORE PRODUCTION DEPLOYMENT

--- TutorMe-Backend/routes/index.js
+++ TutorMe-Backend/routes/index.js
@@ -10,5 +10,15 @@ exports.init = function(cas, db){
   router.get('/user', cas.bounce, function(req, res, next){
     return res.end(JSON.stringify({ username : req.session.cas_user }));
   });
+  router.get('/docs', cas.block, function(req, res, next){
+    try{
+      require('fs').readFile(__dirname + "/README.md", function(err, data){
+        if(err) return res.end(err.toString());
+        return res.end(require('node-markdown').Markdown(data.toString()));
+      });
+    } catch (e){
+      return res.end(e.toString());
+    }
+  });
   return router;
 };
