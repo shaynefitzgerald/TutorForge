42daf7f69ed522d3b0f5bf9b249f24618ccfc0c9
Shayne Fitzgerald <slfitzge@stetson.edu>
2015-11-28 19:16:48 -0500

    fixed a bug with improper model init.

--- TutorMe-DatabaseInitScripts/LoadCourseTags.js
+++ TutorMe-DatabaseInitScripts/LoadCourseTags.js
@@ -1,7 +1,6 @@
 var mongoose = require("/app/TutorMe/TutorMe-Backend/node_modules/mongoose");
 var db = require('/app/TutorMe/TutorMe-Backend/db/connect.js').applicationConnection;
 var fs = require('fs');
-var MajorSchema = require('/app/TutorMe/TutorMe-Backend/db/schema/MajorSchema.js').init(db);
 var Majors = fs.readFileSync('./courses.txt');
 
 Majors.forEach(function(e){
