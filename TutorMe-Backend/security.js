f2519cb329738eabdaafd1a2982ce7980e2973bc
Shayne Fitzgerald <slfitzge@stetson.edu>
2015-12-01 20:55:05 -0500

    logging...

--- TutorMe-Backend/security.js
+++ TutorMe-Backend/security.js
@@ -2,8 +2,6 @@ var init = function(cas, db) {
   return function(req, res, next) {
     if (req.session.cas_user === undefined)
       return next();
-    if (req.session.userPermissions !== undefined || req.session.userPermissions !== "")
-      return next();
     var error = function(res, err) {
       return res.end(JSON.stringify({
         success: false,
@@ -16,12 +14,14 @@ var init = function(cas, db) {
     };
 
 
+
     var StudentModel = db.model('StudentModel');
     var TutorModel = db.model('TutorModel');
     var AdministratorModel = db.model('AdministratorModel');
     return StudentModel.findOne({
       Email : toEmail(req.session.cas_user)
     }, function(err, studentResult) {
+      console.log(studentResult);
       if (err) return error(res, err);
       if (studentResult !== undefined && studentResult !== null) {
         if (req.session.userPermissions === undefined) {
@@ -33,6 +33,7 @@ var init = function(cas, db) {
       return TutorModel.findOne({
         Email : toEmail(req.session.cas_user)
       }, function(err, tutorResult) {
+        console.log(tutorResult);
         if (err) return error(res, err);
         if (tutorResult !== undefined && tutorResult !== null) {
           if (req.session.userPermissions === undefined) {
@@ -44,6 +45,7 @@ var init = function(cas, db) {
         return AdministratorModel.findOne({
           Email : toEmail(req.session.cas_user)
         }, function(err, administratorResult) {
+          console.log(administratorResult);
           if(err) return error(res, err);
           if (administratorResult !== undefined && administratorResult !== null) {
             if (req.session.userPermissions === undefined) {
