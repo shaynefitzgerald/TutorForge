dd8347892de943e889a7ded31db1d4d763c72f15
Shayne Fitzgerald <slfitzge@stetson.edu>
2015-12-04 09:35:47 -0500

    rewrite of refs with a different paradigm.

--- TutorMe-DatabaseInitScripts/createRefs.js
+++ TutorMe-DatabaseInitScripts/createRefs.js
@@ -5,22 +5,32 @@ var fs = require('fs');
 var CourseModel = db.model('CourseModel');
 var StudentModel = db.model("StudentModel");
 
-var linkage = function(course){
-  return function(err, result){
-    if(result.Courses === undefined)
-      result.Courses = [];
-    result.Courses.push(course);
-    return result.save(function(err){
-      if(err){ console.error(err); return process.exit(-1); }
-    });
-  };
+var err_check = function(err){
+  if(err) { console.error(err); process.exit(-1); }
 };
 
-CourseModel.find({}, function(err, courses){
-  if(err){ console.error(err); return process.exit(-1); }
-  for(var x = 0; x < courses.length; x++){
-    for(var si = 0; si < courses[x].Students.length; si++){
-      StudentModel.find({ ID : courses[x].Students[si].StudentID }, linkage(courses[x]._id) );
-    }
-  }
+StudentModel.find({}, function(err, result){
+  err_check(err);
+  result.forEach(function(Student){
+    CourseModel.find({ Students : {
+       $elemMatch : {
+         StudentRef : {
+           $exists : false
+         },
+         StudentID : {
+           $eq : Student.ID
+         }
+       }
+     }
+   }, function(err, Courses){
+     err_check(err);
+     var CourseIds = [];
+     for (var Course of Courses) {
+       CourseIds.push(Course);
+     }
+     StudentModel.update({ID : Student.ID}, {'Courses' : CourseIds}, function(err){
+       err_check(err);
+     });
+   });
+  });
 });
