d5d945577d5dc2e51cc20abe71dc5a6492cd9491
Shayne Fitzgerald <slfitzge@stetson.edu>
2016-01-30 15:15:04 -0500

    fixed a bug where route paths weren't joined properly. Now using path.join

--- TutorMe-Backend/routeLoader.js
+++ TutorMe-Backend/routeLoader.js
@@ -1,12 +1,12 @@
 var fs = require('fs');
 var path = require('path');
 exports.init = function(app, routePrefix, CASInstance, db) {
-  var files = fs.readdirSync(__dirname + "/routes").filter(function(element) {
+  var files = fs.readdirSync(path.join(__dirname, routePrefix)).filter(function(element) {
     if (path.extname(element) === '.js') return true;
     return false;
   });
   for(var x = 0; x < files.length; x++){
     var routePath = '/api/' + path.basename(files[x], '.js');
-    app.use(routePath, require(routePrefix + files[x]).init(CASInstance, db));
+    app.use(routePath, require(path.join(routePrefix, files[x])).init(CASInstance, db));
   }
 };
